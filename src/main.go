f445f61f749e56fe8f5aa8d5208fe27cb69ebe4e
Joseph Murillo <josephmurillo2425@gmail.com>
2021-08-31 23:21:09 -0600

    Arithmetic operations

--- src/main.go
+++ src/main.go
@@ -3,5 +3,62 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola Mundo")
+	// Constants = name and type
+	const pi float64 = 3.14
+	const pi2 = 3.1425
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Variables integer
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+	//fmt.Println("Hola Mundo")
+
+	// Zero values - Default values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area calculator
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado: ", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("Suma: ", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta: ", result)
+
+	// Multiplicacion
+	result = x * y
+	fmt.Println("Multiplicación: ", result)
+
+	// División
+	result = y / x
+	fmt.Println("División:", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo: ", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental: ", x)
+
+	// Decremental
+	x--
+	fmt.Println("Decremental: ", x)
 }
